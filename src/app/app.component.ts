7bdd5337e5185f70493ffc2105a5d543ea513c2e
Miguel-Chaparro <miguel.chapariza@gmail.com>
2023-03-08 23:10:13 -0500

    add Partners and fast test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dommatos SAS';
+  downloadSpeed: string;
+
+  constructor() {
+    this.downloadSpeed = '0 Mbps';
+  }
+
+  downloadSpeedTest() {
+
+    const xhr = new XMLHttpRequest();
+    const startTime = performance.now();
+    xhr.addEventListener('progress', event => {
+      const endTime = performance.now();
+      const duration = (endTime - startTime) / 1000;
+      const downloadSpeed = event.loaded / (event.timeStamp / 1000) / 40240 ; // Descarga en Mbps
+      this.downloadSpeed = downloadSpeed.toFixed(2) + ' Mbps';
+
+    });
+    xhr.open('GET', 'http://speedtest-nyc1.digitalocean.com/100mb.test'); // Cambia la URL a un archivo de prueba que sea adecuado para tus necesidades
+    xhr.send();
+    return false;
+  }
 }
